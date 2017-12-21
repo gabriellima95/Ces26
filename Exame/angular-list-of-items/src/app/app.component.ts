42170290ff6cb0a0ed8d85410fc4520f2f6bc2f5
gabriellima <gabrielkvalknt@gmail.com>
2017-12-21 07:58:08 -0200

    add exame

---
+++ Exame/angular-list-of-items/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items = ["Angular 4", "React", "Underscore"];
+  newItem = "";
+  pushItem = function() {
+  if(this.newItem!= ""){
+  		this.items.push(this.newItem);
+  		this.newItem = "";
+  	}
+  }
+
+  removeItem = function(index){
+  	this.items.splice(index,1);
+  }
+}
